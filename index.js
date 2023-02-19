7f93d860922497bc84ca3d7ce304e57d85fb48bb
neenekras0v <104592677+neenekras0v@users.noreply.github.com>
2023-02-19 12:05:08 +0500

    v3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || 5678;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log(`::> Server is up at port http://localhost:${port}`);
